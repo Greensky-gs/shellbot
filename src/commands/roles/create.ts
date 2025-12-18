b83e221c98c4f6fcdd7a0138addc911406b3112e
Greensky <david.hesliere@gmail.com>
2025-12-18 09:00:49 +0100

    feat(cmd): :sparkles: Make role create

---
+++ src/commands/roles/create.ts
@@ -0,0 +1,85 @@
+import { ShellCommand } from "../../structs/Command";
+import { parseHexColor } from "../../utils/regexes";
+
+export default new ShellCommand({
+    name: 'createrole',
+    description: "Create a role",
+    sudoRequired: true,
+    aliases: ['crrole', 'mkrole'],
+    arguments: [
+        {
+            name: 'name',
+            description: "Name of the role",
+            type: 'string',
+            mandatory: true
+        }
+    ],
+    options: [
+        {
+            doubleDash: true,
+            prefix: 'color',
+            argument: {
+                name: 'color',
+                description: "Color of the role",
+                mandatory: false,
+                type: 'string'
+            }
+        },
+        {
+            doubleDash: true,
+            prefix: 'reason',
+            argument: {
+                name: 'reason',
+                description: "Reason of the creation",
+                mandatory: false,
+                type: 'string'
+            }
+        },
+        {
+            doubleDash: true,
+            prefix: "hoist",
+            argument: {
+                name: "hoist",
+                description: "Role is separated from others",
+                mandatory: false,
+                type: 'presence'
+            }
+        },
+        {
+            doubleDash: true,
+            prefix: 'mention',
+            argument: {
+                name: 'mention',
+                mandatory: false,
+                description: "Role is mentionnable",
+                type: 'presence'
+            }
+        }
+    ]
+}).run(async(opts, msg) => {
+    const name = opts.getArgument('name', 'string', true);
+    const color = opts.getString('color', true) ?? '000';
+    const hoist = opts.present('hoist', true);
+    const mention = opts.present('mention', true);
+    const reason = opts.getString('reason', true) ?? 'N/A';
+
+    const hexColor = parseHexColor(color);
+    if (!hexColor) return msg.reply({
+        content: `\`${color}\` : invalid hex color. Something like : \`#ef93e1\``,
+        allowedMentions: {}
+    }).catch(() => {});
+
+    const role = await msg.guild.roles.create({
+        name,
+        colors: { primaryColor: hexColor[1] },
+        hoist,
+        mentionable: mention,
+        reason
+    }).catch(() => {});
+    if (!role) return msg.reply('Creation failed').catch(() => {});
+
+    msg.reply({
+        content: `<@&${role.id}>`,
+        allowedMentions: {}
+    }).catch(() => {});
+});
--- src/utils/regexes.ts
+++ src/utils/regexes.ts
@@ -5,3 +5,11 @@ export const parseMentionnable = (content: string) => {
     if (!res) return null;
     return [res.groups.id, {'@': 'user', '#': 'channel', '@&': 'role'}[res.groups.type]];
 }
+export const parseHexColor = (content: string): null | [`#${string}`, number] => {
+    const regex =  /#?(?<pattern>([0-9A-F]{3}){1,2})/i;
+    const res = regex.exec(content);
+
+    if (!res) return null;
+    const pattern = ((pat: string) => pat.length === 3 ? pat.concat(pat) : pat)(res.groups.pattern);
+    return [`#${pattern}`, parseInt(pattern, 16)];
+}
